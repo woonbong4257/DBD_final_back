b83ab4e19c90ee9d4bf860947f89c4e6113ee45d
woonbong4257 <unjong0926@gmail.com>
2024-12-09 20:36:04 +0900

    stdMain fin

--- controller/profMain.js
+++ controller/profMain.js
@@ -3,52 +3,29 @@ const pool = require('../DB/db')
 exports.getProfMain = async (req, res) =>{
   try{
     const user = req.session.user;
-    const [selAccept] = await pool.query(`
-      SELECT count(accept) as "acc" FROM mission WHERE professor_pro_id = 101101 AND accept = "수락"
-      `)
-    const [selReject] = await pool.query(`
-      SELECT count(accept) as "rej" FROM mission WHERE professor_pro_id = 101101 AND accept = "거절"
-      `)
-    const [selHold] = await pool.query(`
-      SELECT count(accept) as "hold" FROM mission WHERE professor_pro_id = 101101 AND accept = "보류"
-      `)
-    const [selWait] = await pool.query(` 
-      SELECT count(accept) as "wait" FROM mission WHERE professor_pro_id = 101101 AND accept = "대기"
-      `)
-    //지도학생 미션 수락률
-    const mission = [
-      Number(selAccept[0].acc),
-      Number(selReject[0].rej),
-      Number(selHold[0].hold),
-      Number(selWait[0].wait)
-    ]
-    const [totalLead] = await pool.query(`
-      SELECT avg(compe_up) as "lead" FROM std_compe_up WHERE compe_name = "리더십" AND term = "24-2"
-      `)
-    const [totalPlia] = await pool.query(`
-      SELECT avg(compe_up) as "plia" FROM std_compe_up WHERE compe_name = "유연성" AND term = "24-2"
-      `)
-    const [totalOrig] = await pool.query(`
-      SELECT avg(compe_up) as "orig" FROM std_compe_up WHERE compe_name = "독창성" AND term = "24-2"
-      `)
-    const [totalMento] = await pool.query(`
-      SELECT avg(compe_up) as "mento" FROM std_compe_up WHERE compe_name = "멘토링" AND term = "24-2"
-      `)
-    const [totalSelfDev] = await pool.query(`
-      SELECT avg(compe_up) as "self" FROM std_compe_up WHERE compe_name = "자기개발" AND term = "24-2"
-      `)
-    const [totalColla] = await pool.query(`
-      SELECT avg(compe_up) as "colla" FROM std_compe_up WHERE compe_name = "협동" AND term = "24-2"
-      `)
+    const [[selAccept]] = await pool.query(`
+      SELECT 
+        COUNT(CASE WHEN accept = "수락" THEN 1 END) as "acc",
+        COUNT(CASE WHEN accept = "거절" THEN 1 END) as "rej",
+        COUNT(CASE WHEN accept = "보류" THEN 1 END) as "hold",
+        COUNT(CASE WHEN accept = "대기" THEN 1 END) as "wait"
+      FROM mission 
+      WHERE professor_pro_id = 101101
+    `)
+    
+    const [totalCompe] = await pool.query(`
+      SELECT 
+        FLOOR(AVG(CASE WHEN compe_name = '리더십' THEN compe_up END)) as "lead",
+        FLOOR(AVG(CASE WHEN compe_name = '유연성' THEN compe_up END)) as "plia",
+        FLOOR(AVG(CASE WHEN compe_name = '독창성' THEN compe_up END)) as "orig", 
+        FLOOR(AVG(CASE WHEN compe_name = '멘토링' THEN compe_up END)) as "mento",
+        FLOOR(AVG(CASE WHEN compe_name = '자기개발' THEN compe_up END)) as "self",
+        FLOOR(AVG(CASE WHEN compe_name = '협동' THEN compe_up END)) as "colla"
+      FROM std_compe_up 
+      WHERE term = '24-2'
+    `)
     //전체학생 역량 성장치(진짜 성장치만...)
-    const compeUp = [
-      Math.floor(Number(totalLead[0].lead)), 
-      Math.floor(Number(totalPlia[0].plia)), 
-      Math.floor(Number(totalOrig[0].orig)), 
-      Math.floor(Number(totalMento[0].mento)), 
-      Math.floor(Number(totalSelfDev[0].self)), 
-      Math.floor(Number(totalColla[0].colla))
-    ]
+
     const [stdCompeUp] = await pool.query(`
       SELECT 
         FLOOR(AVG(CASE WHEN compe_name = '리더십' THEN compe_up END)) as "lead",
@@ -65,7 +42,7 @@ exports.getProfMain = async (req, res) =>{
     const [holdMission] = await pool.query(`
       SELECT * FROM hold inner join mission on hold.mission_mis_num = mission.mis_num WHERE mission.professor_pro_id = 101101 AND mission.accept = '보류'
       `)
-    res.send({mission: mission, compeUp: compeUp, stdCompeUp: stdCompeUp, holdMission: holdMission})
+    res.send({selAccept: selAccept, totalCompe: totalCompe, stdCompeUp: stdCompeUp, holdMission: holdMission})
   }
   catch(err){
     console.error(err)
--- controller/stdMain.js
+++ controller/stdMain.js
@@ -4,7 +4,7 @@ exports.getStdMain = async (req, res) =>{
   try{
     const user = req.session.user;
     const [name] = await pool.query(`
-      SELECT name FROM student WHERE std_id = 20200005
+      SELECT name, std_state, grade FROM student WHERE std_id = 20200005
       `, [user])
     //성장치
     const [selCompeUp] = await pool.query(`
@@ -24,13 +24,48 @@ exports.getStdMain = async (req, res) =>{
       `, [user])
     //신청한 프로그램 수 
     const [appliProgram] = await pool.query(`
-      SELECT count(appli_num) FROM program_appli WHERE student_std_id = 20200005 AND progress_state = "완료"
+      SELECT COUNT(appli_num) as "appli"
+      FROM program_appli WHERE student_std_id = 20200005
       `, [user])
+    const [isuProgram] = await pool.query(`
+      SELECT COUNT(appli_num) as "isu"
+      FROM program_appli WHERE student_std_id = 20200005 AND progress_state = "이수"
+      `, [user])
+    const [miisuProgram] = await pool.query(`
+      SELECT COUNT(appli_num) as "miisu"
+      FROM program_appli WHERE student_std_id = 20200005 AND progress_state = "미이수"
+      `, [user])
+      const stdProgram = {appli: appliProgram[0].appli, isu: isuProgram[0].isu, miisu: miisuProgram[0].miisu}
+    const [recommendProgram] = await pool.query(`
+      SELECT * FROM program_appli inner join student ON program_appli.student_std_id = student.std_id 
+      WHERE student.grade = 1 ORDER BY program_appli.appli_num DESC LIMIT 5
+      `, [name.grade])
     // 프로그램 불러오기 limit 수치는 나중에 알려주세요
     const [programList] = await pool.query(`
       SELECT * FROM program ORDER BY id DESC LIMIT 5
       `)
-    res.send({name: name, compeUp: selCompeUp, totalCompe: selTotalCompe, mySeedRank: mySeedRank, appliProgram: appliProgram, programList: programList})
+    const [selAccept] = await pool.query(`
+      SELECT accept FROM mission WHERE student_std_id = 20200005
+      `, [user])
+
+    const response = {
+      name: name,
+      compeUp: selCompeUp,
+      totalCompe: selTotalCompe,
+      mySeedRank: mySeedRank,
+      stdProgram: stdProgram,
+      programList: programList,
+      recommendProgram: recommendProgram,
+    };
+
+    if (selAccept[0].accept == "수락") {
+      const [acceptMission] = await pool.query(`
+        SELECT * FROM mission_compe inner join mission ON mission_compe.mission_mis_num = mission.mis_num WHERE student_std_id = 20200005
+        `, [user]);
+      response.acceptMission = acceptMission;
+    }
+
+    res.send(response);
   }
   catch(err){
     console.error(err)
