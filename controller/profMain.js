3a03a10edbf86d6cb3ddb7bda62913ff78fa3ca7
woonbong4257 <unjong0926@gmail.com>
2024-12-11 01:15:36 +0900

    add prof_name and sum_compe

--- controller/profMain.js
+++ controller/profMain.js
@@ -3,6 +3,10 @@ const pool = require('../DB/db')
 exports.getProfMain = async (req, res) =>{
   try{
     const user = req.session.user
+    //교수 이름
+    const[name] = await pool.query(`
+      SELECT name FROM professor WHERE pro_id = "101101"
+      `, [user])
     //전체학생 역량 성장치
     const [totalCompeUp] = await pool.query(`
       SELECT 
@@ -51,22 +55,29 @@ exports.getProfMain = async (req, res) =>{
       FROM std_compe inner join student on std_compe.student_std_id = student.std_id
       WHERE professor_pro_id = 101101
     `)
-
+    //보류 미션 목록
     const [holdMission] = await pool.query(`
       SELECT hold_num, mission_mis_num, compe_name, compe_figure, hold_figure, hold_date FROM hold inner join mission on hold.mission_mis_num = mission.mis_num 
       WHERE mission.professor_pro_id = 101101 AND mission.accept = '보류'
       `)
+      //보류 학생 역량 합산
+    const [sumFigure] = await pool.query(`
+      SELECT SUM(hold_figure) FROM hold WHERE mission_mis_num = ?
+      `, [holdMission[0].mission_mis_num])
+      //보류 학생 목록
     const [holdStd] = await pool.query(`
-      SELECT student.std_id, student.name student.grade FROM mission inner join student on mission.student_std_id = student.std_id 
+      SELECT student.std_id, student.name ,student.grade FROM mission inner join student on mission.student_std_id = student.std_id 
       WHERE student.professor_pro_id = 101101 AND mission.accept = '보류'
       `)
     
     res.send({
+      name: name,
       totalCompe: totalCompe, 
       totalCompeUp: totalCompeUp, 
       stdCompeUp: stdCompeUp, 
       holdMission: holdMission, 
-      holdStd: holdStd
+      holdStd: holdStd,
+      sumFigure: sumFigure
     })
   }
   catch(err){
