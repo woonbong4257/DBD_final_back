0141b29e87270033e28cfa1791922ae6f2ae2013
woonbong4257 <unjong0926@gmail.com>
2024-12-13 03:04:12 +0900

    get req fin

--- controller/missionProgram.js
+++ controller/missionProgram.js
@@ -6,7 +6,7 @@ exports.postMissionProgram = async (req, res) => {
     const {term} = req.body
     if(term === '24-2'){
     const [missionProgram] = await pool.query(`
-      SELECT * FROM program WHERE appli_date >= '2024-09-01' AND appli_date < '2025-03-02' AND user_id = ?
+      SELECT * FROM program_appli WHERE appli_date >= '2024-09-01' AND appli_date < '2025-03-02' AND student_std_id = '20200005'
     `, [user])
     res.send({missionProgram: missionProgram})
     } 
